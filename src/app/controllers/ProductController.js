a7b2b49abfe55bb2c3906778e847a4872e04a5f2
gabrielcaiana <gabrielcaiana@hotmail.com>
2021-01-05 20:42:47 -0300

    formatando preço do produto

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,3 +1,5 @@
+const { formatPrice } = require("../../lib/utils");
+
 const Category = require("../models/Category");
 const Product = require("../models/Product");
 
@@ -23,10 +25,22 @@ module.exports = {
 
    let results = await Product.create(req.body)
    const productId = results.rows[0].id
-
-   results = await Category.all()
-   const categories = results.rows
-
-   return res.render("products/create.njk", {productId, categories})
+   
+   return res.redirect(`products/${productId}`)
   },
+  
+  async edit(req, res) {
+    let results = await Product.find(req.params.id)
+    const product = results.rows[0]
+
+    product.old_price = formatPrice(product.old_price)
+    product.price = formatPrice(product.price)
+ 
+    results = await Category.all()
+    const categories = results.rows
+
+    if(!product) res.send("Product not found!")
+ 
+    return res.render("products/edit.njk", {product, categories})
+  }
 };
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -21,4 +21,10 @@ module.exports = {
       format: `${day}/${month}/${year}`
     };
   },
+  formatPrice(price) {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL' 
+     }).format(price/100)
+  }
 };

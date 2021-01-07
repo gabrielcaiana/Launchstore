f8c7252948b71d66e98550b25e6c84e67039ed43
gabrielcaiana <gabrielcaiana@hotmail.com>
2021-01-07 16:14:11 -0300

    refatorando gerenciador de uploads

--- public/app.js
+++ public/app.js
@@ -1,49 +1,61 @@
 const Mask = {
-  apply(input, func){
+  apply(input, func) {
     setTimeout(() => {
-      input.value = Mask[func](input.value)
-    }, 1)
+      input.value = Mask[func](input.value);
+    }, 1);
   },
-  formatBRL(value){
+  formatBRL(value) {
     value = value.replace(/\D/g, "");
 
-    return new Intl.NumberFormat('pt-BR', {
-     style: 'currency',
-     currency: 'BRL' 
-    }).format(value/100)
-  }
-}
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(value / 100);
+  },
+};
 
 const PhotosUpload = {
+  preview: document.querySelector("#photos-preview"),
   uploadLimit: 6,
   handlerFileInput(event) {
-    const { files: fileList } = event.target
-    const { uploadLimit } = PhotosUpload
+    const { files: fileList } = event.target;
+    
+    if(PhotosUpload.hasLimit(event)) return true
+
+    Array.from(fileList).forEach((file) => {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        const image = new Image();
+        image.src = String(reader.result);
+
+        const div = PhotosUpload.getContainer(image)
+
+        PhotosUpload.preview.appendChild(div);
+      };
+    });
+  },
+  hasLimit(event) {
+    const { uploadLimit } = PhotosUpload;
 
     if (fileList.length > uploadLimit) {
-      alert(`Envie no máximo ${uploadLimit} fotos`)
-      event.preventDefault()
-      return
+      alert(`Envie no máximo ${uploadLimit} fotos`);
+      event.preventDefault();
+      return true;
     }
 
-    Array.from(fileList).forEach( file => {
-      const reader = new FileReader()
+    return false
+  },
+  getContainer(image) {
+    const div = document.createElement("div");
+    div.classList.add("photo");
 
-      reader.readAsDataURL(file)
-      
-      reader.onload = () => {
-        const image = new Image()
-        image.src = String(reader.result)
-        
-        const div = document.createElement("div")
-        div.classList.add("photo")
-        
-        div.onclick = () => alert("Remover imagem")
+    div.onclick = () => alert("Remover imagem");
 
-        div.appendChild(image)
+    div.appendChild(image);
 
-        document.querySelector("#photos-preview").appendChild(div)
-      }
-    }) 
-  }
-}
+    return div
+  },
+};
